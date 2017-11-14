0e0d3b9daf2557947350412d055007031acf95ce
alekseik1 <alekseik1@yandex.ru>
2017-11-14 10:42:00 +0300

    Адаптировал под python3

--- markov.py
+++ markov.py
@@ -12,6 +12,9 @@ WORD_SEPARATOR = ' '
 
 if __name__ == '__main__':
 	args = sys.argv
+	main(args)
+
+def main(args):
 	usage = 'Usage: %s (parse <name> <depth> <path to txt file>|gen <name> <count>)' % (args[0], )
 
 	if (len(args) < 3):
@@ -19,26 +22,26 @@ if __name__ == '__main__':
 
 	mode  = args[1]
 	name  = args[2]
-	
+
 	if mode == 'parse':
 		if (len(args) != 5):
 			raise ValueError(usage)
-		
+
 		depth = int(args[3])
 		file_name = args[4]
-		
+
 		db = Db(sqlite3.connect(name + '.db'), Sql())
 		db.setup(depth)
-		
+
 		txt = codecs.open(file_name, 'r', 'utf-8').read()
 		Parser(name, db, SENTENCE_SEPARATOR, WORD_SEPARATOR).parse(txt)
-	
+
 	elif mode == 'gen':
 		count = int(args[3])
 		db = Db(sqlite3.connect(name + '.db'), Sql())
 		generator = Generator(name, db, Rnd())
 		for i in range(0, count):
-			print generator.generate(WORD_SEPARATOR)
-	
+			return generator.generate(WORD_SEPARATOR)
+
 	else:
 		raise ValueError(usage)
